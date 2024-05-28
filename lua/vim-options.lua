c2e300c6fa6d4c978b6c6973fe0f9ba0755daa04
Chamal1120 <kenway213beep@gmail.com>
2024-05-28 17:21:43 +0530

    minor changes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
